f4da146cb762797282ea6593c6bb06829a9fa6e6
Fábio da Silva Costa <biophas@gmail.com>
2019-05-29 07:04:01 -0300

    Update
    Update

---
+++ Tópico 201 - Kernel Linux/201.2 - Compilando o Kernel - Configuração .mm
@@ -0,0 +1,140 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1558427938933" ID="ID_202193808" MODIFIED="1558428781320" STYLE="fork" TEXT="201.2 - Compilando o Kernel - Configura&#xe7;&#xe3;o ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="24"/>
+<node CREATED="1558427964590" ID="ID_1269550292" MODIFIED="1558428795639" POSITION="right" STYLE="fork" TEXT="Commands">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="20"/>
+<node CREATED="1558428082964" ID="ID_1130162007" MODIFIED="1558428812010" STYLE="fork" TEXT="make menuconfig">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="16"/>
+<node CREATED="1558428152436" ID="ID_1253247159" MODIFIED="1558428767545" STYLE="fork" TEXT="Baseados em menu">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+<node CREATED="1558428332140" ID="ID_1817149055" MODIFIED="1558428767506" STYLE="fork" TEXT="Enter = Seleciona">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428352772" ID="ID_707575487" MODIFIED="1558428767511" STYLE="fork" TEXT="--&gt; = Submenus">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428365188" ID="ID_1940447923" MODIFIED="1558428767514" STYLE="fork" TEXT="--- = Vazio">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428374565" ID="ID_636981199" MODIFIED="1558428767502" STYLE="fork" TEXT="Y = Inclui">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428383604" ID="ID_732327102" MODIFIED="1558428767518" STYLE="fork" TEXT="N = Exclui">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428390373" ID="ID_571675511" MODIFIED="1558428767521" STYLE="fork" TEXT="&lt;M&gt; = M&#xf3;dulares features ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428407981" ID="ID_1561954554" MODIFIED="1558428767526" STYLE="fork" TEXT="Esc esc = Volta">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428417820" ID="ID_1701554020" MODIFIED="1558428767530" STYLE="fork" TEXT="? = Help">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428428789" ID="ID_658536328" MODIFIED="1558428767533" STYLE="fork" TEXT="&lt;/&gt; = Search">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428438580" ID="ID_1566020392" MODIFIED="1558428767538" STYLE="fork" TEXT="[*] Built-in (integrado ao kernel)">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428463748" ID="ID_1583015373" MODIFIED="1558428767535" STYLE="fork" TEXT="[ ] Exclu&#xed;do ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428482524" ID="ID_333462483" MODIFIED="1558428767541" STYLE="fork" TEXT="&lt; &gt; Module capable">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1558428098596" ID="ID_180571551" MODIFIED="1558428812007" STYLE="fork" TEXT="make config">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="16"/>
+<node CREATED="1558428163988" ID="ID_1742169078" MODIFIED="1558428767560" STYLE="fork" TEXT="Baseado em perguntas ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1558428107437" ID="ID_1372900716" MODIFIED="1558428812006" STYLE="fork" TEXT="make xconfig">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="16"/>
+<node CREATED="1558428177653" ID="ID_1625210929" MODIFIED="1558428767563" STYLE="fork" TEXT="Utiliza o servidor X">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428193957" ID="ID_1287004632" MODIFIED="1558428767565" STYLE="fork" TEXT="Depend&#xea;ncias">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+<node CREATED="1558428233917" ID="ID_1883442762" MODIFIED="1558428767550" STYLE="fork" TEXT="pkg-config, g++, libqt4-dev">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1558428117108" ID="ID_1003737254" MODIFIED="1558428812004" STYLE="fork" TEXT="make gconfig ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="16"/>
+<node CREATED="1558428254278" ID="ID_1899288149" MODIFIED="1558428767568" STYLE="fork" TEXT="Utiliza o servidor X">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+<node CREATED="1558428293957" ID="ID_1782483164" MODIFIED="1558428767574" STYLE="fork" TEXT="Depend&#xea;ncias">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+<node CREATED="1558428302957" ID="ID_310211762" MODIFIED="1558428767572" STYLE="fork" TEXT="gtk+-2.0, gmodule-2.0, libglade2-dev, glib-2.0">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1558427970900" ID="ID_984592270" MODIFIED="1558428795636" POSITION="left" STYLE="fork" TEXT="Configuration Files">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="20"/>
+<node CREATED="1558428514469" ID="ID_560951553" MODIFIED="1558428812001" STYLE="fork" TEXT=".config">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="16"/>
+<node CREATED="1558428624421" ID="ID_1252958485" MODIFIED="1558428767602" STYLE="fork" TEXT="O campo CONFIG_LOCALVERSION pode ser editado com a caracter&#xed;stica da versao Ex. = &quot;-BPHS&quot;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1558428579949" ID="ID_1734014518" MODIFIED="1558428811991" STYLE="fork" TEXT="Makefile">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="16"/>
+<node CREATED="1558428588421" ID="ID_1516270796" MODIFIED="1558428767596" STYLE="fork" TEXT="Pode ser editado colocando o n&#xfa;mero da vers&#xe3;o no campo EXTRAVERSION = -111">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1558427988348" FOLDED="true" ID="ID_41971457" MODIFIED="1558428795641" POSITION="right" STYLE="fork" TEXT="Features">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial" SIZE="20"/>
+<node CREATED="1558428005059" ID="ID_1900719693" MODIFIED="1558428767580" STYLE="fork" TEXT="Depend&#xea;ncias ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+<node CREATED="1558428018197" ID="ID_89971975" MODIFIED="1558428767577" STYLE="fork" TEXT="build-essential, bison, flex, libncurses-dev">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Arial" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1558428075277" ID="ID_991843962" MODIFIED="1558428767580" TEXT=""/>
+</node>
+</node>
+</map>
